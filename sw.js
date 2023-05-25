980552576e3a8941e0875e1120c6c5796f0a982a
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-25 12:27:05 +0530

    Service Worker - Caching static Caching

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 self.addEventListener("install",function(event){
     console.log("installed",event);
+    event.waitUntil(
+        caches.open("static").then((cache)=>{
+            cache.add("/js/jquery-3.5.1.min.js")
+            cache.add("/js/script.js")
+        })
+    );
+   
 });
 
 self.addEventListener("activate",function(event){
