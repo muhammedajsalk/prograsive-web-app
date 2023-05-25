a74b6b4deae38760d8edba716b5ad6b6cc287bdd
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-25 12:35:28 +0530

    ervice Worker - Caching Retrieving Items from the Cache

--- sw.js
+++ sw.js
@@ -15,6 +15,13 @@ self.addEventListener("activate",function(event){
 });
 
 self.addEventListener("fetch",function(event){
-    console.log("Service worker fetch",event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request).then((response)=>{
+          if(response){
+            return response;
+          }else{
+            return fetch(event.request);
+          }
+        })
+    );
 });
