80305b6914e5ac4555523d7009387bd405d4309f
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-25 11:32:47 +0530

    installing service worker

--- js/script.js
+++ js/script.js
@@ -46,3 +46,9 @@ $(document).ready(function () {
         },
     });
 });
+
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("/sw.js").then(function(){
+        console.log("service worker registered!");
+    })
+}
