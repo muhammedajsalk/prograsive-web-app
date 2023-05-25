66464e0604785c55af53559644fe99f7cd6c7824
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-25 11:54:16 +0530

    Non-Lifecycle Event

--- sw.js
+++ sw.js
@@ -6,3 +6,8 @@ self.addEventListener("activate",function(event){
     console.log("Activated",event);
     return self.clients.claim();
 });
+
+self.addEventListener("fetch",function(event){
+    console.log("Service worker fetch",event);
+    event.respondWith(fetch(event.request));
+});
